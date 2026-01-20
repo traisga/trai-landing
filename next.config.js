1ab1c7d5d27f6dadef027494a769fd0c95b79406
traisga <fyrat91@gmail.com>
2026-01-20 05:20:00 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
+cd ~/mobile-projects/trai-landing
+cat > next.config.js << 'EOF'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: {
-      // Uyarı: Bu ayar, projede hata olsa bile build işleminin 
-      // başarıyla tamamlanmasını sağlar.
       ignoreDuringBuilds: true,
-    },
-    output: 'export',
-    trailingSlash: true,
-    images: {
-      unoptimized: true,
     },
   }
   
   module.exports = nextConfig
+EOF
